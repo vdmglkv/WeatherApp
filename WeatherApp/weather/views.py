6427bb7d7304d6be7c6cb27ea001fcf06a2fa2a0
Vadim Gulakov <Haker.201@yandex.ru>
2021-11-06 16:39:55 +0300

    Create main view

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
-    return render(request, 'weather/index.html')
+    appid = 'e8edd503f33a4623b2e6d34e9a1e5c32'
+    city = 'Gomel'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=' + appid
+    res = requests.get(url).json()
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon']
+    }
+
+    content = {'info': city_info}
+
+    return render(request, 'weather/index.html', content)
